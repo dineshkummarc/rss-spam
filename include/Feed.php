54420d1f2a9dde516da7f7f01b96d750f036ffb3
Matt Campbell <matt@mattcampbell.net>
2012-01-22 01:06:23 -0500

    Hooking up Feed to FeedParser

--- include/Feed.php
+++ include/Feed.php
@@ -10,11 +10,27 @@ class Feed {
 		$this->updateInterval = $args['updateInterval'];
 	}
 
+	protected function loadFeed() {
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $this->url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+		$data = curl_exec($curl);
+
+		if (curl_errno($curl)) {
+			return null;
+		}
+
+		return FeedParser::parse($data);
+	}
+
 	public function update($db, $force = false) {
 		if (!$force) {
 			if (!$this->db->shouldFeedUpdate($this->urlHash, $this->updateInterval)) {
 				return false;
 			}
 		}
+
+		return $this->loadFeed();
 	}
 }
