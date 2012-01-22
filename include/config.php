29cce91867c103ede05a547b288562c01bea5fe5
Matt Campbell <matt@mattcampbell.net>
2012-01-22 00:34:43 -0500

    Tweaking config format

--- include/config.php
+++ include/config.php
@@ -1,9 +1,19 @@
 <?
-$dbConfig = array(
-	'host' => 'localhost',
-	'database' => 'rss_spam',
-	'username' => 'root',
-	'password' => ''
-);
+$config = array(
+	'db' => array(
+		'host' => 'localhost',
+		'database' => 'rss_spam',
+		'username' => 'root',
+		'password' => ''
+	),
 
-$feeds = array();
+	'mail' => array(
+		'to' => 'destination@example.com',
+		'from' => 'source@example.com'
+	),
+
+	'feeds' => array(
+		'url' => 'http://example.com/index.rss',
+		'updateInterval' => 3600
+	)
+);
