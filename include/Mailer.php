b77b000b9a7eeaefaced41d1d651cb3ef27501a5
Matt Campbell <matt@mattcampbell.net>
2012-01-22 18:40:44 -0500

    Sending emails as HTML

--- include/Mailer.php
+++ include/Mailer.php
@@ -1,16 +1,23 @@
 <?
 class Mailer {
+	protected $to;
+	protected $from;
+
 	public function __construct($args) {
 		$this->to = $args['to'];
 		$this->from = $args['from'];
 	}
 
 	public function mail($subject, $message) {
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+		$headers .= 'From: ' . $this->from . "\r\n";
+
 		mail(
 			$this->to,
 			$subject,
 			$message,
-			'From: ' . $this->from
+			$headers
 		);
 	}
 }
