463c0ebd50a0ed5e5f7688616e6529e7e14f61b9
Matt Campbell <matt@mattcampbell.net>
2012-01-22 01:56:12 -0500

    Tying it all together

---
+++ main.php
@@ -0,0 +1,21 @@
+<?
+include('include/Database.php');
+include('include/Mailer.php');
+include('include/FeedParser.php');
+include('include/Feed.php');
+include('include/config.php');
+
+$db = new Database($config['db']);
+$mailer = new Mailer($config['mail']);
+
+foreach ($config['feeds'] as $feed) {
+	print 'Processing ' . $feed['url'] . ' ...';
+	$db->ensureFeedExists($feed['url']);
+	$feed = new Feed($feed);
+	if (!$feed->update($db, $mailer)) {
+		print 'Error with ' . $feed['url'] . '!';
+	}
+
+	print "\n";
+}
+
